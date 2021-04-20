9bcbf2503a324aba8ff478eaa2a751e2a3551c7f
Richard Cornish <richardcornish@gmail.com>
2021-04-20 11:53:03 -0700

    Removed requires

--- setup.py
+++ setup.py
@@ -15,9 +15,6 @@ setup(
     zip_safe=False,
     include_package_data=True,
     packages=find_packages(exclude=('tests',)),
-    install_requires=[
-        'django',
-    ],
     test_suite='roman.tests',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
